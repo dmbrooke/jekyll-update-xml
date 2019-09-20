ca62f76c2a79b7b748e2a80a685d20c687c1a245
dbrooke <dbrooke@coveo.com>
2019-09-20 14:55:29 -0400

    Updated script

--- lib/jekyll-update-xml.rb
+++ lib/jekyll-update-xml.rb
@@ -1,55 +1,94 @@
+require "rainbow"
+require "builder"
+require "rexml/document"
+
 module Jekyll
 
     # Finds duplicate slugs, logs those in a file, and outputs a warning
     # message in the console.
-    Jekyll::Hooks.register :site, :pre_render do |site|
+    class JekyllUpdateXmlFeed < Jekyll::Generator
+        safe true
+        priority :lowest
 
-        require "rainbow"
-        require "builder"
-        require "rexml/document"
+        MINIFY_REGEX = %r!(?<=>\n|})\s+!.freeze
 
-        whats_new_collection = site.collections["whats-new"]
-
-        new_update_docs = Array.new
-
-        @output = ""
-        xml = Builder::XmlMarkup.new(:target => @output, :indent => 1)
-
-        # Iteratte  through collection for docs
-        whats_new_collection.docs.each do |doc|
-            if !File.open('./assets/persistentFeed.xml').each_line.any?{|line| line.include?(doc.basename)}
-                new_update_docs << doc
-            end
+        def generate(site)
+            generateXml(site.collections["whats-new"])
+            @site = site
+            @site.pages << persistentFeed unless file_exists?("persistentFeed.txt")
+            @site.pages << feed unless file_exists?("feed.xml")
         end
 
-        if new_update_docs.size > 0
-            puts Rainbow("Updating feed.").yellow
-            xml.instruct!
-            xml.updates do
-                new_update_docs.each do |doc|
-                    xml.update do
-                        xml.collection doc.data["category"]
-                        xml.typeOfChange doc.data["typeOfChange"]
-                        xml.date doc.data["createdDate"]
-                        xml.content doc.content
-                        xml.links do
-                            doc.data["links"].each do |link|
-                                xml.link link
+        def source_path(file = "feed.xml")
+            File.expand_path "../#{file}", __dir__
+        end
+
+        def feed
+            xml_feed =  PageWithoutAFile.new(@site, __dir__, "", "feed.txt")
+            xml_feed.content = File.read(source_path).gsub(MINIFY_REGEX, "")
+            xml_feed.data["layout"] = nil
+            xml_feed
+        end
+
+        def persistentFeed
+            persistent_feed =  PageWithoutAFile.new(@site, __dir__, "", "persistentFeed.txt")
+            persistent_feed.content = File.read(source_path("persistentFeed.txt")).gsub(MINIFY_REGEX, "")
+            persistent_feed.data["layout"] = nil
+            persistent_feed
+        end
+
+        def pages_and_files
+            @pages_and_files ||= @site.pages + @site.static_files
+        end
+
+        # Checks if a file already exists in the site source
+        def file_exists?(file_path)
+            pages_and_files.any? { |p| p.url == "/#{file_path}" }
+        end
+
+
+        def generateXml(whats_new_collection)
+            new_update_docs = Array.new
+
+            @output = ""
+            xml = Builder::XmlMarkup.new(:target => @output, :indent => 1)
+
+            # Iteratte  through collection for docs
+            whats_new_collection.docs.each do |doc|
+                if !File.open(source_path('persistentFeed.txt')).each_line.any?{|line| line.include?(doc.basename)}
+                    new_update_docs << doc
+                end
+            end
+
+            if new_update_docs.size > 0
+                puts Rainbow("Updating feed.").yellow
+                xml.instruct!
+                xml.updates do
+                    new_update_docs.each do |doc|
+                        xml.update do
+                            xml.collection doc.data["category"]
+                            xml.typeOfChange doc.data["typeOfChange"]
+                            xml.date doc.data["createdDate"]
+                            xml.content doc.content
+                            xml.links do
+                                doc.data["links"].each do |link|
+                                    xml.link link
+                                end
                             end
                         end
                     end
                 end
-            end
 
-            # Write the update(s) to the XML feed
-            open('./assets/feed.xml', 'w') do |line|
-                line.puts @output
-            end
+                # Write the update(s) to the XML feed
+                open(source_path, 'w') do |line|
+                    line.puts @output
+                end
 
-            # Update the persisted files
-            open('./assets/persistentFeed.xml', 'w') do |line|
-                whats_new_collection.docs.each { |doc| line.puts(doc.basename)  }
+                # Update the persisted files
+                open(source_path('persistentFeed.txt'), 'w') do |line|
+                    whats_new_collection.docs.each { |doc| line.puts(doc.basename)  }
+                end
             end
         end
     end
-end
+end`
